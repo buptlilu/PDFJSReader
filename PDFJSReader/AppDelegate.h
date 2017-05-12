f07b34f0391156ece2fa478722cfe7498976b050
lilu <chujunhe1234@163.com>
2017-05-12 16:38:59 +0800

    init

---
+++ PDFJSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFJSReader
+//
+//  Created by lilu on 2017/5/12.
+//  Copyright © 2017年 lilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDFJSReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDFJSReader
+//
+//  Created by lilu on 2017/5/12.
+//  Copyright © 2017年 lilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
